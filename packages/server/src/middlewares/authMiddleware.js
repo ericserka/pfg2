0304598b75d6504951e2fcf28fadf87927b97d0b
Eric Serka <ericserka@hotmail.com>
2022-11-21 13:18:42 -0300

    refactor: using ReasonPhrases

--- packages/server/src/middlewares/authMiddleware.js
+++ packages/server/src/middlewares/authMiddleware.js
@@ -1,16 +1,16 @@
-import { StatusCodes } from 'http-status-codes'
-import { verifyToken } from '../services/authService.js'
+import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import { ErrorHandler } from '../helpers/errors.js'
+import { verifyToken } from '../services/authService.js'
 
 export const authMiddleware = (req, res, next) => {
   const token = req?.headers?.authorization
   if (!token) {
-    throw new ErrorHandler(StatusCodes.UNAUTHORIZED, 'Unauthorized')
+    throw new ErrorHandler(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED)
   }
   try {
     req.user = verifyToken(token.split('Bearer')[1].trim())
     next()
   } catch (_error) {
-    throw new ErrorHandler(StatusCodes.UNAUTHORIZED, 'Unauthorized')
+    throw new ErrorHandler(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED)
   }
 }
