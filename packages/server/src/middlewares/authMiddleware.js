cce086e58c86c9c0bca3d79d038ce961626ada36
ericserka <ericserka@hotmail.com>
2022-11-21 13:22:52 -0300

    fix: replacing unauthorized with nao autorizado

--- packages/server/src/middlewares/authMiddleware.js
+++ packages/server/src/middlewares/authMiddleware.js
@@ -1,16 +1,16 @@
-import { ReasonPhrases, StatusCodes } from 'http-status-codes'
+import { StatusCodes } from 'http-status-codes'
 import { ErrorHandler } from '../helpers/errors.js'
 import { verifyToken } from '../services/authService.js'
 
 export const authMiddleware = (req, res, next) => {
   const token = req?.headers?.authorization
   if (!token) {
-    throw new ErrorHandler(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED)
+    throw new ErrorHandler(StatusCodes.UNAUTHORIZED, 'Não autorizado')
   }
   try {
     req.user = verifyToken(token.split('Bearer')[1].trim())
     next()
   } catch (_error) {
-    throw new ErrorHandler(StatusCodes.UNAUTHORIZED, ReasonPhrases.UNAUTHORIZED)
+    throw new ErrorHandler(StatusCodes.UNAUTHORIZED, 'Não aurotizado')
   }
 }
